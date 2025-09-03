5f171a01463c07e4a05cab8599b9e7fdee998311
noda <sono4.sono18@gmail.com>
2025-09-04 04:45:13 +0900

    DB初期化2

--- backend/src/init-db.ts
+++ backend/src/init-db.ts
@@ -1,23 +1,24 @@
 import pool from './db/index';
-
+const dropTableQuery = `
+  DROP TABLE IF EXISTS "Ranking";
+`;
+/*
 const createTableQuery = `
   CREATE TABLE IF NOT EXISTS "Ranking" (
-    "Rank" SERIAL PRIMARY KEY,
+    "ID" SERIAL PRIMARY KEY,
     "Team_name" VARCHAR(15) NOT NULL,
     "score" INT NOT NULL
   );
 `;
-
+*/
 const initializeDatabase = async () => {
   try {
     const client = await pool.connect();
-    console.log('データベースの初期化を開始します...');
-    await client.query(createTableQuery);
-    console.log('テーブル "Ranking" が正常に作成または確認されました。');
+  //  console.log('データベースの初期化を開始します...');
+  //  await client.query(createTableQuery);
+  //  console.log('テーブル "Ranking" が正常に作成または確認されました。');
     
-    // もし初期データを入れたい場合は、ここに追加します
-    // 例: await client.query('INSERT INTO "Ranking" ("Team_name", "score") VALUES ($1, $2)', ['Team A', 100]);
-    
+    await client.query(dropTableQuery);
     client.release();
     console.log('データベースの初期化が完了しました。');
   } catch (err) {
