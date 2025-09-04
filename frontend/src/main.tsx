7f253b537bfb3d67114826167edebe9eeca677fe
noda <sono4.sono18@gmail.com>
2025-09-05 07:21:43 +0900

    ユーザーIDまわりのフロントエンド改修2

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import { PlayerIdProvider } from './contexts/PlayerIdContext.tsx'; // ✨ インポート
+import { PlayerIdProvider } from './contexts/PlayerIdContext'; // ✨ インポート
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
